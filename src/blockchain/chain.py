58b58f6a184018111635aed30f24a3722f18db9b
Alireza <alirzahosinopoor.1390@gmail.com>
2025-07-14 19:52:38 +0330

    Updated chain

--- src/blockchain/chain.py
+++ src/blockchain/chain.py
@@ -37,20 +37,32 @@ class Blockchain:
 
     def add_block(self, data):
         last_block = self.get_last_block()
+        if not last_block:
+            raise Exception("Blockchain not initialized")
+        
         new_block = Block(
             index=last_block.index + 1,
             data=data,
             previous_hash=last_block.hash
         )
+
         new_block = self.proof_of_work(new_block)
+
+        save_block(new_block)
         self.chain.append(new_block)
-        self.save_chain()
+
         return new_block
 
     def proof_of_work(self, block, difficulty=3):
+        start_time = time.time()
+        print(f"Mining block #{block.index}...")
+        
         while not block.hash.startswith('0' * difficulty):
             block.nonce += 1
             block.hash = block.calculate_hash()
+        
+        mining_time = time.time() - start_time
+        print(f"Block mined in {mining_time:.2f}s | Hash: {block.hash}")
         return block
 
     def save_chain(self):
@@ -68,53 +80,71 @@ class Blockchain:
 
     def register_node(self, address):
         parsed_url = urlparse(address)
-        self.nodes.add(parsed_url.netloc)
+        if not parsed_url.netloc:
+            raise ValueError("Invalid node address")
+        add_node(parsed_url.netloc)
 
     def chain_validate(self, chain=None):
         chain = chain or self.chain
-        previous_block = chain[0]
-        current_index = 1
-
-        while current_index < len(chain):
-            block = chain[current_index]
-
-            if previous_block != previous_block.hash:
-                return False
+        if not chain:
+            return False
             
-            if not block.hash.startswith('0' * 3):
-                return False
+        genesis = chain[0]
+        if genesis.index != 0 or genesis.previous_hash != "0":
+            return False
             
-            if block.hash != block.calculate_hash():
-                return False
+        for i in range(1, len(chain)):
+            current = chain[i]
+            previous = chain[i-1]
             
-            previous_block = block
-            current_index += 1
-
+            if current.index != previous.index + 1:
+                return False
+                
+            if current.previous_hash != previous.hash:
+                return False
+                
+            if current.hash != current.calculate_hash():
+                return False
+                
+            if not current.hash.startswith('0' * 3):
+                return False
+                
         return True
     
     def resolve_conflicts(self):
+        nodes = get_nodes()
+        if not nodes:
+            return False
+            
         new_chain = None
         max_length = len(self.chain)
 
-        for node in self.nodes:
+        for node in nodes:
             try:
-                response = requests.get(f'http://{node}/chain')
+                response = requests.get(f'http://{node}/chain', timeout=3)
                 if response.status_code == 200:
-                    length = response.json()['length']
-                    chain_data = response.json()['chain']
-                    
-                    if length > max_length:
-                        chain = [Block.from_dict(block) for block in chain_data]
+                    chain_data = response.json().get('chain', [])
+                    if len(chain_data) > max_length:
+                        chain = []
+                        for block_data in chain_data:
+                            chain.append(Block(
+                                index=block_data['index'],
+                                timestamp=block_data['timestamp'],
+                                data=block_data['data'],
+                                previous_hash=block_data['previous_hash'],
+                                nonce=block_data['nonce'],
+                                hash=block_data['hash']
+                            ))
                         
                         if self.is_chain_valid(chain):
-                            max_length = length
                             new_chain = chain
-            except requests.exceptions.RequestException:
+                            max_length = len(chain)
+            except (requests.RequestException, ValueError, KeyError):
                 continue
         
         if new_chain:
+            replace_chain(new_chain)
             self.chain = new_chain
-            self.save_chain()
             return True
             
         return False
